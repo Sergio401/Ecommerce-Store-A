5f29dc1f01e1f4f5efa8ce9f257d95ad356108f1
Sergio Gonzalez <sergioleonardo.gonzalez@gmail.com>
2021-04-18 13:06:02 -0500

    Render by dom manipulation - first test

--- js/loadStore.js
+++ js/loadStore.js
@@ -1,23 +1,18 @@
-import { games } from "./games"
-
 class GamesCover {
     constructor() {
-        this.cardsContainer = document.querySelector(".trendings")
-        this.card = document.createElement("div")
-        this.image = document.createElement("img")
-        this.title = document.createElement("h2")
-        this.date = document.createElement("span")
+        this.cardsContainer = document.querySelector(".trending")
+        this.card = document.createElement("li")
+        this.img = document.createElement("img")
+        this.cardInfo = document.createElement("div")
+        this.title = document.createElement("p")
+
+
 
         this.createCards()
     }
 
     createCards() {
-        games.forEach(game => {
-            this.cardContainer = this.cardsContainer.appendChild(this.card.cloneNode(true))
-            this.cardContainer.appendChild(this.image.cloneNode(true)).innerHTML = game.image
-            this.cardContainer.appendChild(this.title.cloneNode(true)).innerHTML = game.title
-            this.cardContainer.appendChild(this.date.cloneNode(true)).innerHTML = game.price
-        })
+        this.cardsContainer.appendChild(this.card)
     }
 }
 
