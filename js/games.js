6a5b56130d6e1972e9ae9480315545b9c3368c3b
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2021-04-18 08:04:37 -0500

    Se realizo el objeto con información de los juegos, titulo, precio, etc.

---
+++ js/games.js
@@ -0,0 +1,444 @@
+const games = [
+
+    /* LATEST GAMES ---------------------------------------------------*/
+    {
+        title: "The Last Of Us 2",
+        price: "35,67",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_933361-MCO42268907714_062020-O.webp",
+        description: "The Last of Us narra la historia de Joel, un sobreviviente que pierde a su familia y que eventualmente se encuentra con Ellie"},
+
+    {
+        title: "Ghost Of Tsushima Ps4",
+        price: "55,07",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_969697-MCO42697870061_072020-O.webp",
+        description: "Ambientada en el Japón feudal en el que un samurái trata de hacer frente a la invasión del ejército mongol en el año 1274."},
+
+    {
+        title: "Resident Evil 7 Gold Edition Ps4",
+        price: "31,80",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_794308-MCO41536056988_042020-O.webp",
+        description: "Es un videojuego perteneciente al género de horror de supervivencia"},
+
+    {
+        title: "Call Of Duty Black Ops Cold War",
+        price: "82,99",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_769740-MCO43739032261_102020-O.webp",
+        description: "Es un videojuego de disparos en primera persona"},
+
+    {
+        title: "Watch Dogs Legión Ps4",
+        price: "62,24",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_904737-MCO44081716697_112020-O.webp",
+        description: "El grupo de hackers DedSec une esfuerzos para combatir un régimen autoritario que ha tomado el control del Reino Unido"},
+
+    {
+        title: "Spiderman Miles Morales Ps5",
+        price: "69,16",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_965045-MCO44740833238_012021-O.webp",
+        description: "es un videojuego de acción y aventuras desarrollado"},
+
+    {
+        title: "Assassins Creed Valhalla",
+        price: "69,16",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_694864-MCO44728757039_012021-O.webp",
+        description: "Conviértete en Eivor, un poderoso saqueador vikingo y lidera a tu clan desde las inclementes costas de Noruega a un nuevo hogar"},
+
+    {
+        title: "Little Nightmares",
+        price: "22,11",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_825697-MCO26388108702_112017-O.webp",
+        description: "Una hambrienta niña de nueve años llamada Six es secuestrada en Las Fauces, un misterioso barco de abastecimiento para el capricho de locas y poderosas criaturas."},
+
+    {
+        title: "Cyberpunk 2077 Xbox One",
+        price: "74,72",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_933395-MCO44375675905_122020-O.webp",
+        description: "Es una historia de acción y aventuras en mundo abierto ambientada en Night City"},
+
+    {
+        title: "Mortal Kombat 11 Xbox One",
+        price: "35,39",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_995799-MCO30057424199_042019-O.webp",
+        description: "Una legendaria historia en la que los luchadores del pasado de Mortal Kombat se enfrentan a los del presente"},
+    
+
+
+    /* TRENDINGS GAMES ----------------------------------------------------*/
+   
+
+     {
+        title: "The Last Of Us 2",
+        price: "35,67",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_933361-MCO42268907714_062020-O.webp",
+        description: "The Last of Us narra la historia de Joel, un sobreviviente que pierde a su familia y que eventualmente se encuentra con Ellie"},
+
+    {
+        title: "Call Of Duty Black Ops Cold War",
+        price: "82,99",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_769740-MCO43739032261_102020-O.webp",
+        description: "Es un videojuego de disparos en primera persona"},
+
+    {
+        title: "Mortal Kombat 11 Xbox One",
+        price: "35,39",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_995799-MCO30057424199_042019-O.webp",
+        description: "Una legendaria historia en la que los luchadores del pasado de Mortal Kombat se enfrentan a los del presente"},
+
+    {
+        title: "Little Nightmares 2",
+        price: "68,91",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_784057-MCO44776931265_022021-O.webp",
+        description: "es una aventura de terror que nos llevará a un mundo gigantesco y grotesco para resolver puzles,"},
+
+    {
+        title: "Assassins Creed Valhalla",
+        price: "69,16",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_694864-MCO44728757039_012021-O.webp",
+        description: "Conviértete en Eivor, un poderoso saqueador vikingo y lidera a tu clan desde las inclementes costas de Noruega a un nuevo hogar"},
+
+    {
+        title: "Spiderman Miles Morales Ps5",
+        price: "69,16",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_965045-MCO44740833238_012021-O.webp",
+        description: "es un videojuego de acción y aventuras desarrollado"},
+
+    {
+        title: "Halo The Master Chief Collection",
+        price: "35.98",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_892615-MCO25283772166_012017-O.webp",
+        description: "Es un recopilatorio con Halo: Combat Evolved, Halo 2, Halo 3 y Halo 4"},
+
+    {
+        title: "Forza Horizon 4",
+        price: "47,02",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_643786-MCO44511248324_012021-O.webp",
+        description: "Vive la vida de Horizon mientras participas en el festival automotriz más grande del mundo"},
+
+    {
+        title: "Spiderman PS4 Juego del año",
+        price: "40,10",
+        list: "TRENDINGS GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_973923-MCO32556875703_102019-O.webp",
+        description: "En Spider-Man te esperan saltos, combos de combates, carreras y muchas, muchas telarañas."},
+    
+
+    {
+        title: "Cyberpunk 2077 Xbox One",
+        price: "74,72",
+        list: "LATEST GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_933395-MCO44375675905_122020-O.webp",
+        description: "Es una historia de acción y aventuras en mundo abierto ambientada en Night City"},
+    
+
+    
+    /* XBOX GAMES -------------------------------------------*/
+
+     {
+        title: "Grand Theft Auto V",
+        price: "31,80",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_771484-MCO41821465223_052020-O.webp",
+        description: "Experimenta Grand Theft Auto V, el mundo abierto de Rockstar Games aclamado por la crítica"},
+
+    {
+        title: "Mortal Kombat 11 Xbox One",
+        price: "35,39",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_995799-MCO30057424199_042019-O.webp",
+        description: "Una legendaria historia en la que los luchadores del pasado de Mortal Kombat se enfrentan a los del presente"},
+
+    {
+        title: "Resident Evil 2 Remake",
+        price: "41,48",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_786663-MCO41485282459_042020-O.webp",
+        description: "Una reinvención escalofriante de un clásico de terror: basado en el lanzamiento original de la consola PlayStation en 1998"},
+
+    {
+        title: "Resident Evil 3 Remake",
+        price: "63,07",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_677030-MCO40596379164_012020-O.webp",
+        description: "El brote de pesadilla del virus T, un arma biológica desarrollada por la compañía farmacéutica Umbrella Corporation."},
+
+    {
+        title: "Halo The Master Chief Collection",
+        price: "35.98",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_892615-MCO25283772166_012017-O.webp",
+        description: "Es un recopilatorio con Halo: Combat Evolved, Halo 2, Halo 3 y Halo 4"},
+
+    {
+        title: "Minecraft",
+        price: "10,38",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_674064-MCO43645090598_102020-O.webp",
+        description: "La propuesta invita a los jugadores a sobrevivir, crear y explorar un mundo abierto"},     
+        
+    {
+        title: "Minecraft Dungeons",
+        price: "12,45",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_611562-MCO41996244836_052020-O.webp",
+        description: "Será un juego de rastreo de mazmorras con monstruos generados aleatoriamente."},
+
+    {
+        title: "Gears 5",
+        price: "20,48",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_825806-MCO44311577103_122020-O.webp",
+        description: "Es un videojuego de género shooter en tercera persona."},
+
+    {
+        title: "Ori And The Blind Forest Definitive",
+        price: "15.99",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_876397-MCO44562303166_012021-O.webp",
+        description: "Ayudará a Ori a recolectar fragmentos de vida, fragmentos de energía, nuevas habilidades y mejoras."},
+
+    {
+        title: "Forza Horizon 4",
+        price: "47,02",
+        list: "XBOX GAMES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_643786-MCO44511248324_012021-O.webp",
+        description: "Vive la vida de Horizon mientras participas en el festival automotriz más grande del mundo"},
+
+   /*  PLAY STATION GAMES -----------------------------------------*/
+
+    {
+        title: "Uncharted 4 PS4",
+        price: "24,88",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_808941-MCO31353716562_072019-O.webp",
+        description: "Ambientado tres años después de los eventos de Uncharted 3"}, 
+    
+    {
+        title: "Horizon Zero Dawn",
+        price: "21,86",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_604391-MCO27729847872_072018-O.webp",
+        description: "Ve más allá de las montañas del norte y explora un páramo helado que oculta una nueva amenaza"}, 
+    
+    {
+        title: "Gog Of War",
+        price: "24,88",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_782205-MCO40292161426_122019-O.webp",
+        description: "un nuevo comienzo para el personaje y una ambientación nórdica"},
+    
+
+    {
+        title: "The Last Of Us 2",
+        price: "35,67",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_933361-MCO42268907714_062020-O.webp",
+        description: "The Last of Us narra la historia de Joel, un sobreviviente que pierde a su familia y que eventualmente se encuentra con Ellie"},
+
+    {
+        title: "FIFA 21",
+        price: "38,72",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_797451-MCO43385280863_092020-O.webp",
+        description: "FIFA 21 es un videojuego de fútbol del año 2020"},
+
+    {
+        title: "Spiderman PS4 Juego del año",
+        price: "40,10",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_973923-MCO32556875703_102019-O.webp",
+        description: "En Spider-Man te esperan saltos, combos de combates, carreras y muchas, muchas telarañas."},
+
+    {
+        title: "Assassins Creed Origins",
+        price:  "27,40",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_973595-MCO26280144166_112017-O.webp",
+        description: "El escenario de tu historia será el Antiguo Egipto, el lugar más misterioso de la historia."},
+
+    {
+        title: "Marvel Vs Capcom Infinite",
+        price: "55,07",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_902298-MCO29746786435_032019-O.webp",
+        description: " El choque épico entre dos universos vuelve pisos con Marvel vs Capcom: Infinito."},
+
+    {
+        title: "Call Of Duty Infinite Warfare Legacy",
+        price: "33,18",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_892872-MCO28295761583_102018-O.webp",
+        description: "batallas masivas y una historia de defensa de nuestro modelo de vida amenazado por el Settlement Defense Front."},
+    
+
+    {
+        title: "Far Cry 4",
+        price: "12,15",
+        list: "PLAY STATION",
+        image: "https://http2.mlstatic.com/D_NQ_NP_855033-MCO27455135838_052018-O.webp",
+        description: "Far Cry 4 se desarrolla en una región ficticia del Himalaya llamada Kyrat, un país probablemente basado en Nepal o Bután."},
+
+
+
+  /*   FANS GEAR ---------------------------------------------------- */
+
+   {
+        title: "Halo Infinite John-117 Color Block Tee",
+        price: "24,99",
+        list: "FANS GEAR",
+        image: "https://cdn.shopify.com/s/files/1/0427/7095/6453/products/halo-shirt-front_1800x1800.jpg?v=1603467474",
+        description: "Prepárate para Halo Infinite con nuestra última colección diseñada en colaboración con Meta Threads."},
+
+    {
+        title: "Xbox Series X Color Block Tee",
+        price: "29,99",
+        list: "FANS GEAR",
+        image: "https://cdn.shopify.com/s/files/1/0427/7095/6453/products/Xbox_Series_X_Color_Block_Tee_Front_01_1800x1800.jpg?v=1604535329",
+        description: "inspirada en la Serie X diseñada en colaboración con Meta Threads."},
+
+    {
+        title: "Gears of War Hivebusters Hoodie",
+        price: "39.99 ",
+        list: "FANS GEAR",
+        image: "https://cdn.shopify.com/s/files/1/0427/7095/6453/products/HD22797GOWUFront_1800x1800.jpg?v=1603894696",
+        description: "Elegante sudadera con capucha y cremallera. Con un diseño original de Gears of War"},
+
+    {
+        title: "Wasteland 3 Logo Hoodie",
+        price: "41.99",
+        list: "FANS GEAR",
+        image: "https://cdn.shopify.com/s/files/1/0427/7095/6453/products/Wasteland3-Logo-100002-MF_1800x1800.png?v=1613599380",
+        description: "Protéjase del perpetuo invierno nuclear del postapocalipsis mientras lleva el logo de un videojuego que le guste."},
+
+    {
+        title: "Gears Tactics x Luke Preece Tee",
+        price: "19.99",
+        list: "FANS GEAR",
+        image: "https://cdn.shopify.com/s/files/1/0427/7095/6453/products/TS636556GOWFront_1800x1800.jpg?v=1603894781",
+        description: "Esta camiseta presenta al héroe de Gears Tactics, Gabe Diaz, con guiños a su historia como parte del grupo de motores de la COG."},   
+
+    {
+        title: "Assassin's Creed - Valhalla Buso",
+        price: "27,12",
+        list: "FANS GEAR",
+        image: "https://http2.mlstatic.com/D_NQ_NP_647455-MCO42547126469_072020-O.webp",
+        description: "Elaborado con Materiales de alta calidad"},
+
+    {
+        title: "Buzo Con Capota - League Of Legends",
+        price:"24,91",
+        list: "FANS GEAR",
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_930474-MCO31110823598_062019-F.webp",
+        description: "Elaborado con Materiales de alta calida"},
+
+    {
+        title: "Buzo Con Capota Games - Apex Legends Games",
+        price: "24,91",
+        list: "FANS GEAR",
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_676206-MCO31557983616_072019-F.webp",
+        description: "Elaborado con Materiales de alta calida"},
+
+    {
+        title: "Buzo con capota Sonic",
+        price: "23,52",
+        list: "FANS GEAR",
+        image: "https://http2.mlstatic.com/D_NQ_NP_884314-MCO45594804528_042021-O.webp",
+        description: "Buzo Hoodie Chompa Algodon Personalizado Video Juegos Gamer"},
+
+    {
+        title: "Buzo Marvel Con Capota",
+        price: "20,76",
+        list: "FANS GEAR",
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_673523-MCO45550207461_042021-F.webp",
+        description: "Elaborado con Materiales de alta calida"}, 
+
+
+    /*   ACCESSORIES ---------------------------------------------------- */
+
+    {
+        title: "Control Xbox One black inalámbrico ",
+        price: "67,80",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_948446-MLA45317859166_032021-O.webp",
+        description: "Experimenta la comodidad y la sensación de confort que siempre buscaste."},
+
+    {
+        title: "Control Xbox One night ops special edition",
+        price: "77,21",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_627251-MLA45317849365_032021-O.webp",
+        description: "Experimenta la comodidad y la sensación de confort que siempre buscaste."},
+
+    {
+        title: "Control Xbox battlegrounds limited edition",
+        price: "65,99",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_854915-MLA45317840395_032021-O.webp",
+        description: "Disfruta del diseño con superficies esculpidas y una geometría refinada para una mayor comodidad durante el juego."},
+
+    {
+        title: "Control Xbox One lunar white special edition",
+        price: "65,99",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_727831-MLA45317842353_032021-O.webp",
+        description: "Te permite jugar sin necesidad de que haya cables de por medio."},
+
+    {
+        title: "Control Xbox One cyberpunk 2077 limited edition",
+        price: "67,99",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_854223-MLA45317859223_032021-O.webp",
+        description: "Mantén el objetivo con el agarre texturizado y mando de dirección híbrido."},
+
+    {
+        title: "Control Ps 4 Dualshock 4 glacier white",
+        price: "69,16",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_672790-MLA45321089242_032021-O.webp",
+        description: "Con Sony también sentirás vibraciones mucho más intensas."},
+
+    {
+        title: "Control Ps 4 Dualshock 4 gold",
+        price: "71,40",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_759351-MLA45321113046_032021-O.webp",
+        description: "Toma el control con más comodidad, disfruta de un sonido de alta fidelidad y gana precisión"},    
+
+    {
+        title: "Control Ps 4 Dualshock 4 green camouflage",
+        price: "72,78",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_814339-MLA45321031876_032021-O.webp",
+        description: "Cuatro luces LED de color ofrecen una forma más sencilla de identificar a los jugadores"},
+
+    {
+        title: "Control Ps 4 Dualshock 4 midnight blue",
+        price: "91,30",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_903706-MLA45321085248_032021-O.webp",
+        description: "Te permite jugar sin necesidad de que haya cables de por medio."},
+
+    {
+        title: "Control joystick inalámbrico Sony Dualshock 4 rose gold",
+        price: "80,23",
+        list: "ACCESSORIES",
+        image: "https://http2.mlstatic.com/D_NQ_NP_928040-MLA45321023980_032021-O.webp",
+        description: "Cuenta con conexión Bluetooth de alta tecnología para usarlo en cualquier ordenador o dispositivo"},
+] 
